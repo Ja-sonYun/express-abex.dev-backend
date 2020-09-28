034c9c8332d5b05994951e6ad99d1819f7c25976
JasonYun <jasonyun@JasonYuns-MacBook-Pro.local>
2020-09-29 07:48:45 +0900

    change function name

--- routes/projects.js
+++ routes/projects.js
@@ -6,7 +6,7 @@ var REST = require('../config/database.js');
 
 router.get('/get', function(req, res, next) {
 	let getResult = new REST();
-	getResult.execute('SELECT * FROM PROJECTS', res);
+	getResult.executeAndReturn('SELECT * FROM PROJECTS', res);
 });
 
 module.exports = router;
