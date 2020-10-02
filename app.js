e90497ed9f85c0a08b9e1e372ca414bedb3e444b
Ubuntu <ubuntu@ip-172-31-45-37.ap-northeast-1.compute.internal>
2020-10-02 00:48:56 +0000

    allow cors for all origins

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors({
-	origin: 'https://abex.dev'
-}));// enable cors
-//app.use(cors());
+//app.use(cors({
+//	origin: 'https://abex.dev'
+//}));// enable cors
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
