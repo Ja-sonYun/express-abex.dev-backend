17b358b4615b5ff4292c22799fcfec655c9321a1
JasonYun <jasonyun@JasonYuns-MacBook-Pro.local>
2020-10-10 11:52:49 +0900

    seperate first setup and insert

---
+++ insertExistProjects.sql
@@ -0,0 +1,6 @@
+USE aws_abex;
+
+INSERT INTO PROJECTS (title, url, description, status) VALUES ('Image Merger', 'imagemerger.abex.dev', 'Get all images from given url and merge them to an image. (React)', 'Done..?');
+INSERT INTO PROJECTS (title, url, description, status) VALUES ('Online Hex Editor', 'hexeditor.abex.dev', 'Online Hex Editor with hexadecimal-decimal-binary-ascii converter. (Vue)', 'On progress');
+INSERT INTO PROJECTS (title, url, description, status) VALUES ('Exif Analyzer', 'exifanalyzer.abex.dev', 'Extract exif data from given jpeg file and update it. (Vue)', 'On progress');
+INSERT INTO PROJECTS (title, url, description, status) VALUES ('blog..?', 'api.abex.dev/ms', 'My laravel application. Kind of community site.', 'Done');
--- mysql_first_setup.sql
+++ mysql_first_setup.sql
@@ -27,7 +27,3 @@ CREATE TABLE PROJECT_TODOS (
 	FOREIGN KEY (project_id) REFERENCES PROJECTS(project_id) ON DELETE CASCADE
 );
 
-INSERT INTO PROJECTS (title, url, description, status) VALUES ('Image Merger', 'imagemerger.abex.dev', 'Get all images from given url and merge them to an image. (React)', 'Done..?');
-INSERT INTO PROJECTS (title, url, description, status) VALUES ('Online Hex Editor', 'hexeditor.abex.dev', 'Online Hex Editor with hexadecimal-decimal-binary-ascii converter. (Vue)', 'On progress');
-INSERT INTO PROJECTS (title, url, description, status) VALUES ('Exif Analyzer', 'exifanalyzer.abex.dev', 'Extract exif data from given jpeg file and update it. (Vue)', 'On progress');
-INSERT INTO PROJECTS (title, url, description, status) VALUES ('blog..?', 'api.abex.dev/ms', 'My laravel application. Kind of community site.', 'Done');
