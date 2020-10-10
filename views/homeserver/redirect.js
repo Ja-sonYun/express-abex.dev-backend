eb0665b2590b415f2f088e94429159c64894fcb1
JasonYun <jasonyun@JasonYuns-MacBook-Pro.local>
2020-10-10 16:04:25 +0900

    added loading page

--- views/homeserver/redirect.js
+++ views/homeserver/redirect.js
@@ -1,4 +1,6 @@
 axios.post("/ms/creatingnewurl").then((res) => {
+	document.getElementById("toptext").innerHTML = 'Done!';
+	document.getElementById("bottomtext").innerHTML = 'Now redirecting to the gernerated url...';
 	window.location.href = res.data.url;
 }).catch((err) => {
 	alert(`something wrong! please contact to here -> admin@abex.dev`);
