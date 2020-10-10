9902d5a1115c50c10c2d8f5b138de10c1a9deb74
JasonYun <jasonyun@JasonYuns-MacBook-Pro.local>
2020-10-10 11:57:33 +0900

    added redirection message

--- views/homeserver/redirect.js
+++ views/homeserver/redirect.js
@@ -1,3 +1,4 @@
+alert("currently trying to request new link from remote.it. please wait for 2 to 10 seconds.");
 axios.post("/ms/creatingnewurl").then((res) => {
 	window.location.href = res.data.url;
 });
