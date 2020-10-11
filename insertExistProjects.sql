04f8937aec6c66ee9637369ada3affc8d7d019e4
Ja-sonYun <ubuntu@ip-172-31-45-37.ap-northeast-1.compute.internal>
2020-10-11 14:37:11 +0900

    added new project

--- insertExistProjects.sql
+++ insertExistProjects.sql
@@ -1,7 +1,8 @@
 USE aws_abex;
 
-INSERT INTO PROJECTS (title, url, description, status) VALUES ('abex.dev', 'abex.dev', 'This website. (React)', 'ON PROGRESS');
-INSERT INTO PROJECTS (title, url, description, status) VALUES ('Image Merger', 'imagemerger.abex.dev', 'Get all images from given url and merge them to an image. (React)', 'Done..?');
-INSERT INTO PROJECTS (title, url, description, status) VALUES ('Online Hex Editor', 'hexeditor.abex.dev', 'Online Hex Editor with hexadecimal-decimal-binary-ascii converter. (Vue)', 'On progress');
-INSERT INTO PROJECTS (title, url, description, status) VALUES ('Exif Analyzer', 'exifanalyzer.abex.dev', 'Extract exif data from given jpeg file and update it. (Vue)', 'On progress');
-INSERT INTO PROJECTS (title, url, description, status) VALUES ('Blog', 'api.abex.dev/ms', 'My laravel application. (laravel 7 & Vue.js) ', 'Done');
+--INSERT INTO PROJECTS (title, url, description, status) VALUES ('abex.dev', 'abex.dev', 'This website. (React)', 'ON PROGRESS');
+--INSERT INTO PROJECTS (title, url, description, status) VALUES ('Image Merger', 'imagemerger.abex.dev', 'Get all images from given url and merge them to an image. (React)', 'Done..?');
+--INSERT INTO PROJECTS (title, url, description, status) VALUES ('Online Hex Editor', 'hexeditor.abex.dev', 'Online Hex Editor with hexadecimal-decimal-binary-ascii converter. (Vue)', 'On progress');
+--INSERT INTO PROJECTS (title, url, description, status) VALUES ('Exif Analyzer', 'exifanalyzer.abex.dev', 'Extract exif data from given jpeg file and update it. (Vue)', 'On progress');
+--INSERT INTO PROJECTS (title, url, description, status) VALUES ('Blog', 'api.abex.dev/ms', 'My laravel application. (laravel 7 & Vue.js) ', 'Done');
+--INSERT INTO PROJECTS (title, url, description, status) VALUES ('Limited Access Link Generator', 'lalg.abex.dev', 'Generate limited access link. If you set the limit 2, generated link will be disconnected from origin url when 2 people visit its url.', 'Done..?');
