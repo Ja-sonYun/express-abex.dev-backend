b42136b6a3d1e1e51435595d2f622d790c924a7e
Ja-sonYun <killa30867@gmail.com>
2020-12-27 20:23:08 +0900

    MODIFY: redirect message for lalg

--- views/laug/redirect.js
+++ views/laug/redirect.js
@@ -1,6 +1,6 @@
 const ORIGIN = document.title;
 
-if(confirm("We're collecting visitors ip addresses for prevent multiple access. If you don't want to pass your ip address to our server, please click { Cancel } button. To continue, please click { OK } button. Ip addresses will be deleted when the target url is expired.")) {
+if(confirm("click ok button to move. You can visit this website only one time.")) {
 	window.location.href = "https://api.abex.dev/laug/l/" + ORIGIN + '/go';
 } else {
 	window.history.back();
